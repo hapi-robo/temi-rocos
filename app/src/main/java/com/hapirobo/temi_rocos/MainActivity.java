af08920d35f37d81f65d43f786cb6ab14aaf8e6b
Raymond Oung <r.oung@hapi-robo.com>
2021-02-17 08:56:00 +0900

    Initial commit

--- app/src/main/java/com/hapirobo/temi_rocos/MainActivity.java
+++ app/src/main/java/com/hapirobo/temi_rocos/MainActivity.java
@@ -10,7 +10,10 @@ import android.util.Log;
 
 import com.robotemi.sdk.BatteryData;
 import com.robotemi.sdk.Robot;
+import com.robotemi.sdk.UserInfo;
 import com.robotemi.sdk.listeners.OnRobotReadyListener;
+
+import java.util.List;
 
 import io.rocos.agent.sdk.AgentSdk;
 import io.rocos.agent.sdk.models.SdkConfig;
@@ -19,19 +22,20 @@ public class MainActivity extends AppCompatActivity implements
         OnRobotReadyListener {
     private static final String TAG = "MAIN";
 
-    private static final String BATTERY_TOPIC = "/battery";
+    private static final String TOPIC_INFO = "/info";
+    private static final String TOPIC_BATTERY = "/battery";
+    private static final String TOPIC_TELEMETRY = "/telemetry";
 
     private static Robot sRobot;
     private static Handler sHandler = new Handler();
 
-    private Runnable batteryPublisher = new Runnable() {
+    private Runnable dataPublisher = new Runnable() {
         @Override
         public void run() {
-            Log.i(TAG, "Publish battery status");
-            sHandler.postDelayed(this, 3000);
+            sHandler.postDelayed(this, 1000);
 
             try {
-                MainActivity.this.robotPublishBatteryStatus();
+                MainActivity.this.robotPublishData();
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
@@ -57,7 +61,20 @@ public class MainActivity extends AppCompatActivity implements
         sRobot = Robot.getInstance();
 
         // run publisher
-        sHandler.post(batteryPublisher);
+        sHandler.post(dataPublisher);
+    }
+
+    public static class RobotInfo {
+        String serialNumber;
+        String launcherVersion;
+        String roboxVersion;
+        List<String> waypointList;
+    }
+
+    public static class RobotTelemetry {
+        int sensor1;
+        int sensor2;
+        int sensor3;
     }
 
     @Override
@@ -86,16 +103,37 @@ public class MainActivity extends AppCompatActivity implements
             } catch (PackageManager.NameNotFoundException e) {
                 throw new RuntimeException(e);
             }
+
+            // get all contacts
+            UserInfo admin = sRobot.getAdminInfo();
+            Log.i(TAG, admin.getName());
+            Log.i(TAG, admin.getUserId());
+            Log.i(TAG, admin.getPicUrl());
+            Log.i(TAG, String.valueOf(admin.getRole()));
         }
     }
 
     //----------------------------------------------------------------------------------------------
     // PUBLISHERS
     //----------------------------------------------------------------------------------------------
-    public void robotPublishBatteryStatus() throws IllegalAccessException {
-        BatteryData batteryData = sRobot.getBatteryData();
-        if (batteryData != null) {
-            AgentSdk.getInstance().publishSource(BATTERY_TOPIC, batteryData);
+    public void robotPublishData() throws IllegalAccessException {
+        Log.i(TAG, "Publish robot data");
+
+        RobotInfo info = new RobotInfo();
+        info.serialNumber = sRobot.getSerialNumber();
+        info.launcherVersion = sRobot.getLauncherVersion();
+        info.roboxVersion = sRobot.getRoboxVersion();
+        info.waypointList = sRobot.getLocations();
+        AgentSdk.getInstance().publishSource(TOPIC_INFO, info);
+
+        BatteryData battery = sRobot.getBatteryData();
+        if (battery != null) {
+            AgentSdk.getInstance().publishSource(TOPIC_BATTERY, battery);
         }
+
+        RobotTelemetry telem = new RobotTelemetry();
+        telem.sensor1 = 1;
+        telem.sensor2 = 2;
+//        AgentSdk.getInstance().publishSource(TOPIC_TELEMETRY, telem);
     }
 }
